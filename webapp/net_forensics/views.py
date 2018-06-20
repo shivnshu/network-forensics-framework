a1450402ac3ba810f1f88290196f58f377a09851
Shivanshu Singh <shivnshus@gmail.com>
2018-06-20 19:42:19 +0530

    Add static files in webapp

--- webapp/net_forensics/views.py
+++ webapp/net_forensics/views.py
@@ -4,4 +4,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    context = {}
+    return render(request, 'index.html', context)
--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'net_forensics.apps.NetForensicsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

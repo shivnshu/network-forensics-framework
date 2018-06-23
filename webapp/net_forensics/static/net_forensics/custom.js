07a3015e2f00a6e3f098cfb32bc270dff96e7c7c
Shivanshu Singh <shivnshus@gmail.com>
2018-06-23 18:39:45 +0530

    Add fusion charts

--- webapp/net_forensics/static/net_forensics/custom.js
+++ webapp/net_forensics/static/net_forensics/custom.js
@@ -6,14 +6,4 @@ $(document).ready(function(){
         document.getElementById('customFileLabel').innerHTML = file.name;
     }
 
-    var advAnalysis = function() {
-        //alert(this.value);
-        location.href = this.value + '.html';
-    }
- 
-    var btnsAnalyse = document.getElementsByClassName("btn-analyse");
-    for (var i = 0; i < btnsAnalyse.length; i++) {
-        btnsAnalyse[i].addEventListener('click', advAnalysis, false);
-    }
-
 });
